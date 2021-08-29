8b7eb635a6c4aa3ed000c269ef4bcc28c915ff7e
angelagongli <angelagongli@gmail.com>
2021-08-29 10:29:42 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { fromEvent, animationFrameScheduler } from 'rxjs';
+import { map, switchMap, takeUntil, subscribeOn } from 'rxjs/operators';
 
 function App() {
+  // From Nicholas Cunningham's guide to Drag and Drop on Medium
+  const box = document.querySelector<HTMLDivElement>('.draggableBox');
+
+  const mousedown$ = fromEvent(box, 'mousedown');
+  const mousemove$ = fromEvent(document, 'mousemove');
+  const mouseup$ = fromEvent(box, 'mouseup');
+
+  const drag$ = mousedown$.pipe(
+    switchMap(
+      (start: MouseEvent) => {
+        return mousemove$.pipe(map((move: MouseEvent) => {
+          move.preventDefault();
+          return {
+            left: move.clientX - start.offsetX,
+            top: move.clientY - start.offsetY
+          }
+        }),
+        takeUntil(mouseup$));
+      }));
+
+  const position$ = drag$.pipe(subscribeOn(animationFrameScheduler));
+
+  position$.subscribe((pos: { top: number; left: number; }) => {
+    box!.style.top = `${pos.top}px`
+    box!.style.left = `${pos.left}px`
+  });
+
   return (
     <div className="App">
       <header className="App-header">
