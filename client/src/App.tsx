4da7a438fd283d4a5e8ef5b74202b7e0ddc18f8b
angelagongli <angelagongli@gmail.com>
2021-10-13 18:19:09 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,24 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import { fromEvent, animationFrameScheduler } from 'rxjs';
 import { map, switchMap, takeUntil, subscribeOn } from 'rxjs/operators';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 function App() {
   // From Nicholas Cunningham's guide to Drag and Drop on Medium
@@ -80,6 +98,37 @@ function App() {
           </a>
         </span>
       </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
