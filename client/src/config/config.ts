2014bd5193655fd5cb9604b4c33641f131cfb862
angelagongli <angelagongli@gmail.com>
2022-02-21 20:02:01 -0600

    Add config.ts

---
+++ client/src/config/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    firebaseConfig: {
+        apiKey: "",
+        authDomain: "audiophile-277609.firebaseapp.com",
+        projectId: "audiophile-277609",
+        storageBucket: "audiophile-277609.appspot.com",
+        messagingSenderId: "",
+        appId: ""
+    }
+};
