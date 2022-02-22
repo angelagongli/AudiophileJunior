93a4d8df5a2de08fa2f4731985c4038bdb97b215
angelagongli <angelagongli@gmail.com>
2022-02-21 20:00:23 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,132 +7,157 @@ import { fromEvent, animationFrameScheduler } from 'rxjs';
 import { map, switchMap, takeUntil, subscribeOn } from 'rxjs/operators';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link
 } from "react-router-dom";
+import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+import AuthRoute from './components/AuthRoute';
+import { initializeApp } from 'firebase/app';
+import { config } from './config/config';
 
-function Home() {
-  return <h2>Home</h2>;
-}
+initializeApp(config.firebaseConfig);
 
-function About() {
-  return <h2>About</h2>;
-}
+export interface IAppProps {};
 
-function Users() {
-  return <h2>Users</h2>;
-}
+// function Home() {
+//   return <h2>Home</h2>;
+// }
 
-function App() {
-  // From Nicholas Cunningham's guide to Drag and Drop on Medium
-  const box = document.querySelector<HTMLDivElement>('.draggableBox');
+// function About() {
+//   return <h2>About</h2>;
+// }
 
-  const mousedown$ = fromEvent(box, 'mousedown');
-  const mousemove$ = fromEvent(document, 'mousemove');
-  const mouseup$ = fromEvent(box, 'mouseup');
+// function Users() {
+//   return <h2>Users</h2>;
+// }
 
-  const drag$ = mousedown$.pipe(
-    switchMap(
-      (start: MouseEvent) => {
-        return mousemove$.pipe(map((move: MouseEvent) => {
-          move.preventDefault();
-          return {
-            left: move.clientX - start.offsetX,
-            top: move.clientY - start.offsetY
-          }
-        }),
-        takeUntil(mouseup$));
-      }));
+// function App() {
+//   // From Nicholas Cunningham's guide to Drag and Drop on Medium
+//   const box = document.querySelector<HTMLDivElement>('.draggableBox');
 
-  const position$ = drag$.pipe(subscribeOn(animationFrameScheduler));
+//   const mousedown$ = fromEvent(box, 'mousedown');
+//   const mousemove$ = fromEvent(document, 'mousemove');
+//   const mouseup$ = fromEvent(box, 'mouseup');
 
-  position$.subscribe((pos: { top: number; left: number; }) => {
-    box!.style.top = `${pos.top}px`
-    box!.style.left = `${pos.left}px`
-  });
+//   const drag$ = mousedown$.pipe(
+//     switchMap(
+//       (start: MouseEvent) => {
+//         return mousemove$.pipe(map((move: MouseEvent) => {
+//           move.preventDefault();
+//           return {
+//             left: move.clientX - start.offsetX,
+//             top: move.clientY - start.offsetY
+//           }
+//         }),
+//         takeUntil(mouseup$));
+//       }));
 
+//   const position$ = drag$.pipe(subscribeOn(animationFrameScheduler));
+
+//   position$.subscribe((pos: { top: number; left: number; }) => {
+//     box!.style.top = `${pos.top}px`
+//     box!.style.left = `${pos.left}px`
+//   });
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Counter />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <span>
+//           <span>Learn </span>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             React
+//           </a>
+//           <span>, </span>
+//           <a
+//             className="App-link"
+//             href="https://redux.js.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Redux
+//           </a>
+//           <span>, </span>
+//           <a
+//             className="App-link"
+//             href="https://redux-toolkit.js.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Redux Toolkit
+//           </a>
+//           ,<span> and </span>
+//           <a
+//             className="App-link"
+//             href="https://react-redux.js.org/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             React Redux
+//           </a>
+//         </span>
+//       </header>
+//       <Router>
+//         <div>
+//           <nav>
+//             <ul>
+//               <li>
+//                 <Link to="/">Home</Link>
+//               </li>
+//               <li>
+//                 <Link to="/about">About</Link>
+//               </li>
+//               <li>
+//                 <Link to="/users">Users</Link>
+//               </li>
+//             </ul>
+//           </nav>
+
+//           <Routes>
+//             <Route path="/about">
+//               <About />
+//             </Route>
+//             <Route path="/users">
+//               <Users />
+//             </Route>
+//             <Route path="/">
+//               <Home />
+//             </Route>
+//           </Routes>
+//         </div>
+//       </Router>
+//       <Navigation />
+//     </div>
+//   );
+// }
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-      <Navigation />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthRoute>
+              <HomePage />
+            </AuthRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
