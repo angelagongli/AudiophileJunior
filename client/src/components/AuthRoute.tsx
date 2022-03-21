b02e523cd6c753ebeed322647905a7713ecb720b
angelagongli <angelagongli@gmail.com>
2022-03-20 21:59:19 -0500

    Update AuthRoute.tsx

--- client/src/components/AuthRoute.tsx
+++ client/src/components/AuthRoute.tsx
@@ -9,31 +9,54 @@ const AuthRoute: React.FunctionComponent<IAuthRouteProps> = props => {
     const auth = getAuth();
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
-    const [loggedIn, setLoggedIn] = useState(false);
+    // const [loggedIn, setLoggedIn] = useState(false);
 
     useEffect(() => {
         console.log("Rendering...");
-        AuthCheck();
+        // Copy AuthCheck fix by the Nerdy Canuck, defining the AuthCheck function in the effect:
+        // https://github.com/joeythelantern/React-Firebase-9/commit/f74a731b772a12953f2bb430d0a8397fe8409b83
+        const AuthCheck = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log("Logged in");
+                setLoading(false);
+                // Why does he setLoading to false when we have not yet set it to true?
+                // setLoggedIn(true);
+            } else {
+                console.log("Unauthorized");
+                // setLoggedIn(false);
+                navigate('/login');
+            }
+        });
+
         return () => {
             console.log("Cleaning up...");
             AuthCheck();
         }
-    }, [loggedIn]);
+    }, [auth]);
+
+    // useEffect(() => {
+    //     console.log("Rendering...");
+    //     AuthCheck();
+    //     return () => {
+    //         console.log("Cleaning up...");
+    //         AuthCheck();
+    //     }
+    // }, [loggedIn]);
     /* Why does auth being in the dependency array completely following the
     Video tutorial by the Nerdy Canuck on YouTube still not force the page to
     Render upon signout? */
 
-    const AuthCheck = onAuthStateChanged(auth, (user) => {
-        if (user) {
-            console.log("Logged in");
-            setLoading(false);
-            setLoggedIn(true);
-        } else {
-            console.log("Unauthorized");
-            setLoggedIn(false);
-            navigate('/login');
-        }
-    });
+    // const AuthCheck = onAuthStateChanged(auth, (user) => {
+    //     if (user) {
+    //         console.log("Logged in");
+    //         setLoading(false);
+    //         // setLoggedIn(true);
+    //     } else {
+    //         console.log("Unauthorized");
+    //         // setLoggedIn(false);
+    //         navigate('/login');
+    //     }
+    // });
 
     if (loading) {
         return <p>Loading...</p>;
