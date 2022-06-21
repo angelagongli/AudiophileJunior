db4aece55599c308f6c85043317ce667e51199c6
angelagongli <angelagongli@gmail.com>
2022-06-20 23:27:40 -0500

    Update AuthRoute

--- client/src/components/AuthRoute.tsx
+++ client/src/components/AuthRoute.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import API from "../utils/API";
+import UserContext from '../utils/UserContext';
 
 export interface IAuthRouteProps {};
 
@@ -9,18 +11,33 @@ const AuthRoute: React.FunctionComponent<IAuthRouteProps> = props => {
     const auth = getAuth();
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
+    const [ourUser, setOurUser] = useState({});
     // const [loggedIn, setLoggedIn] = useState(false);
 
     useEffect(() => {
         console.log("Rendering...");
         // Copy AuthCheck fix by the Nerdy Canuck, defining the AuthCheck function in the effect:
         // https://github.com/joeythelantern/React-Firebase-9/commit/f74a731b772a12953f2bb430d0a8397fe8409b83
-        const AuthCheck = onAuthStateChanged(auth, (user) => {
-            if (user) {
+        const AuthCheck = onAuthStateChanged(auth, (authUser) => {
+            if (authUser) {
                 console.log("Logged in");
                 setLoading(false);
                 // Why does he setLoading to false when we have not yet set it to true?
                 // setLoggedIn(true);
+                API.getUserByAuthUID(authUser.uid)
+                .then(res => {
+                    if (res.data.length) {
+                        console.log("Set the user here: ");
+                        setOurUser(res.data[0]);
+                    } else {
+                        API.makeUser({
+                            AuthUID: authUser.uid,
+                            AuthDisplayName: authUser.displayName
+                        }).then(res => {
+                            console.log("Created new user in db!");
+                        });
+                    }
+                }).catch(err => console.log(err));
             } else {
                 console.log("Unauthorized");
                 // setLoggedIn(false);
@@ -62,7 +79,7 @@ const AuthRoute: React.FunctionComponent<IAuthRouteProps> = props => {
         return <p>Loading...</p>;
     }
 
-    return <>{children}</>;
+    return <UserContext.Provider value={ourUser}>{children}</UserContext.Provider>;
 }
 
 export default AuthRoute;
