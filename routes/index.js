946c18713cf956479512af153a5d247b4e13ca97
angelagongli <angelagongli@gmail.com>
2022-06-20 23:22:21 -0500

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 
-// router.use("/api", apiRoutes);
-
-const cnx = require("../dbconfig/connection");
-const tdsRequest = require('tedious').Request;
+router.use("/api", apiRoutes);
 
 /* I need to send user.uid from firebase/auth to some route to find the subusers,
 because you could be the guardian/babysitter etc. of more than one kid,
 or you could be an adult learning music too */
-
-router.route("/test")
-.get(function(req, res) {
-    res.json("test");
-});
 
 router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
