06ab1e4bcf7f9d078c60549536d9dc572a594f87
angelagongli <angelagongli@gmail.com>
2022-06-20 23:20:59 -0500

    Update db connection

--- dbconfig/connection.js
+++ dbconfig/connection.js
@@ -11,10 +11,16 @@ const config = {
             it fails with sqlcmd too, I can only query the db locally with Windows auth */
             userName: 'angela',
             password: 'password'
+            /* This was as simple as specifying SQL Server and Windows Authentication mode
+            and not the Windows Authentication mode that was set in the MSSQL installation. */
         }
     },
     options: {
-      database: 'master'
+        database: 'master',
+        trustServerCertificate: true,
+        /* With such a small request usually for a kid's app, turning this on
+        will not cause a memory problem, and looking at the rows can be helpful: */
+        rowCollectionOnRequestCompletion: true
     }
 };
 
@@ -27,5 +33,7 @@ connection.on('connect', function(err) {
         console.log('Connected');
     }
 });
+
+connection.connect();
 
 module.exports = connection;
